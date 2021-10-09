3384cd35f0907b9dd9c3a4efe54d9c315fb1f967
Robert Di Pardo <59004801+rdipardo@users.noreply.github.com>
2021-10-09 17:07:58 -0400

    Don't use <stdint.h> when targeting strict ISO C on GNU toolchains
    Up to now we unconditionally included the C99 <stdint.h> header
    and assumed the 'long long' type would be available everywhere,
    making the '%ll' format specifier the default for 64-bit ints.
    
    All safe and correct assumptions in this day and age, except it
    prevents consuming programs from being strict ANSI C.
    
    For those using GNU compilers (GCC and, especially, MinGW), this
    guards the inclusion of <stdint.h> and provides polyfill for the
    int types we need.
    
    There are *no* plans to adapt this hack for MSVC, which didn't
    even define __STDC_VERSION__ before version 16.6 of Visual Studio
    2019 [1]. Apple Clang pulls in the standard int types from a
    collection of BSD headers and should not be affected in any case.
    
    ----
    [1] https://docs.microsoft.com/en-us/cpp/overview/visual-cpp-language-conformance?view=msvc-160#note_C

--- include/moreinttypes/_compat.h
+++ include/moreinttypes/_compat.h
@@ -9,7 +9,9 @@
 #ifndef COMPAT_H
 #define COMPAT_H 1
 
-#if (__cplusplus || __STDC_VERSION__ < 199901L) && !defined(_MSC_VER)
+#define STRICT_ANSI (__STDC_VERSION__ < 199901L)
+
+#if (__cplusplus || STRICT_ANSI) && !defined(_MSC_VER)
 #define restrict __restrict__
 #define inline __inline
 #endif /* (__cplusplus || C89/90) && !MSVC */
@@ -22,27 +24,39 @@
 #endif /* MSVC with `/Tc` compiler switch */
 #endif /* MSVC */
 
+#if !STRICT_ANSI || _MSC_VER || __MACH__
+/* rely on standard type definitions when they're guaranteed to be available */
+
 #define __STDC_LIMIT_MACROS
 #include <inttypes.h> /* <stdint.h> */
 #include <limits.h>
-
-#if __STDC_VERSION__ >= 199901L
 #include <stdbool.h>
 #else
-typedef unsigned char uint8_t;
 #ifndef __cplusplus
+/* polyfill for <stdbool.h> */
+
+typedef unsigned char uint8_t;
 typedef uint8_t bool;
 /* clang-format off */
 #define false ((uint8_t)0)
 /* clang-format on */
 #define true !false
+
+#if STRICT_ANSI && (__GNUC__ || __MINGW32__)
+/* polyfill for <stdint.h>/<inttypes.h> */
+
+typedef int int32_t;
+typedef long int64_t;
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+#endif /* !<stdint.h> && !long long */
 #endif /* !bool */
-#endif /* C89/90 */
+#endif /* strict C89/90 */
 
 /***********************************************************
 *** Match print format specifiers to expected bit-widths ***
 ***********************************************************/
-#if __STDC_VERSION__ >= 199901L
+#if !STRICT_ANSI
 #define BIT_PTR_FMT "%hhu"
 #else
 #define BIT_PTR_FMT "%hu"
@@ -51,8 +65,8 @@ typedef uint8_t bool;
 #define SHRT_PTR_FMT "%h"
 #define U_SHRT_PTR_FMT "%hu"
 
-#if (_WIN64 || __LP64__ || _LP64) && (__MINGW32__ || __MACH__)
-/* 64-bit macOS X or MinGW-w64 Environment */
+#if (_WIN64 || __LP64__ || _LP64) && (__MACH__ || (__MINGW32__ && !STRICT_ANSI))
+/* 64-bit macOS X or C99 compiler targeting a 64-bit MinGW-w64 Environment */
 
 #define SIZE_T_FMT "%llu"
 #define INT_PTR_FMT "%lld"
@@ -77,11 +91,12 @@ typedef uint8_t bool;
 
 #define SIZE_T_FMT "%u"
 #define INT_PTR_FMT "%d"
-#define INT64_PTR_FMT "%lld"
+#define INT64_PTR_FMT "%ld"
 #endif
 
 #if __MINGW32__ && !(__USE_MINGW_ANSI_STDIO || __USE_MINGW_ANSI_STDIO == 0)
 #define __USE_MINGW_ANSI_STDIO 1
 #endif /* __MINGW32__ */
 
+#undef STRICT_ANSI
 #endif /* !COMPAT_H */

a3f685982c845d7498105bc6178dda212ad87679
Robert Di Pardo <59004801+rdipardo@users.noreply.github.com>
2023-10-08 20:04:28 -0400

    Release 1.7.0

--- doxygen/gendocs.cmd
+++ doxygen/gendocs.cmd
@@ -4,8 +4,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`findstr /R "^.*[0-9]\." ..\pkgconfig\moreint
 SET "VERSION_STR=%%F"
 )
 SET "DOXYGEN_PROJECT_VERSION=%VERSION_STR:~9%"
-doxygen -u Doxyfile
 doxygen Doxyfile html
 cd ..
-pyw -m http.server --directory docs/html 2>NUL
+python3 -m http.server --directory docs/html 2>NUL:
 ENDLOCAL
